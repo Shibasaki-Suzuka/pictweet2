7b9ae30a09aa51392a194801d7bfafcc6a02aad3
suzuka <nekonosiawase25@gmail.com>
2023-10-04 19:27:20 +0900

    tweet_destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,12 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  # どのツイートか特定する場合はparams[:id]を使用
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
